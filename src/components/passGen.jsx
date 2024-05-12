c88087315215be86951c2b623ef736141355481a
Akash Kumar Tiwary <btech10172.22@bitmesra.ac.in>
2024-05-12 16:48:18 +0530

    added pass Gen

---
+++ src/components/passGen.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import { useState, useRef, useEffect, useCallback} from 'react';
+const PassGen = () => {
+    const [len, setLen] = useState(8);
+    const [numAll, setNumAll] = useState(false);
+    const [charAll, setCharAll] = useState(false);
+    const [pass, setPass] = useState("");
+
+    const passRef = useRef(null);
+
+    const passGen = useCallback(() => {
+            let pass = "";
+            let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            if(numAll) str += "0123456789";
+            if(charAll) str += "~!@#$%^&*_-+=[]{}><";
+            for(let i =0; i<len; i++){
+                let char = str[Math.floor(Math.random()*str.length)];
+                pass += char;
+            }
+            setPass(pass);
+        }, [len, numAll, charAll, setPass]);
+    
+    const copyPas = useCallback(() => {
+            passRef.current?.select();
+            window.navigator.clipboard.writeText(pass);
+        }, [pass])
+
+
+    useEffect(()=>{
+        passGen();
+    },[len, numAll, charAll, passGen]);
+
+    return (
+    <div className='h-screen w-full bg-amber-500 flex flex-col justify-center items-center'>
+        <h1 className="my-5 text-3xl font-bold underline flex flex-wrap justify-center align-center">my-password-generator</h1>
+        <div className='my-5 border-2 border-cyan-950 p-2'>
+            <input
+            type="text"
+            placeholder='password'
+            value={pass}
+            ref={passRef}
+            />
+            <button className='bg-blue-200 border-2 border-lime-400 p-1 ml-2' 
+            onClick={passGen}>
+                Gen New
+            </button>
+            <button className='bg-blue-200 border-2 border-lime-400 p-1 ml-2' 
+            onClick={copyPas}>
+                copy
+            </button>
+        </div>
+        <div className='my-5'>
+            <input 
+            type="range" 
+            id="length" 
+            className='mx-2 cursor-pointer'
+            min={6}
+            max={100}
+            value = {len}
+            onChange={(e) => setLen(e.target.value)} />
+            <label htmlFor="length">Length : {len}</label>
+            <input 
+            type="checkbox" 
+            id="numbers" 
+            className='mx-2'
+            defaultChecked = {numAll}
+            onClick={()=>{setNumAll((prev) => !prev)}}
+            />
+            <label htmlFor="numbers">numbers</label>
+            <input 
+            type="checkbox" 
+            id="characters"
+            className='mx-2'
+            defaultChecked = {charAll}
+            onClick={() => {setCharAll((prev) => !prev)}}
+            />
+            <label htmlFor="characters">characters</label>
+        </div>
+    </div>
+  )
+}
+
+export default PassGen
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Counter from './components/counter.jsx'
 import BgChanger from './components/bgChanger.jsx'
+import PassGen from './components/passGen.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Counter/>
     <BgChanger/>
+    <PassGen />
   </React.StrictMode>,
 )
