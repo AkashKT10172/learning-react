5a2651dc11671786141d48fa7ab37b2ba365b0ff
Akash Kumar Tiwary <btech10172.22@bitmesra.ac.in>
2024-05-12 16:51:57 +0530

    not

--- src/components/bgChanger.jsx
+++ src/components/bgChanger.jsx
@@ -9,7 +9,7 @@ function BgChanger() {
                 <h1 className="text-3xl font-bold underline flex flex-wrap justify-center">my-background-changer</h1>
                 <h3 className="flex flex-wrap justify-center my-5">Current color is = {color}</h3>
                  <div className="flex flex-wrap justify-center my-5" style={{backgroundColor : color}}>
-                    <button className="outline w-20 h-10 rounded-full shadow-lg mx-2" onClick = {() => setColor("purple")} style={{backgroundColor : "purple"}}>Purple</button>
+                    <button className="outline w-20 h-10 rounded-full shadow-lg mx-2" onClick = {() => setColor("red")} style={{backgroundColor : "red"}}>red</button>
                     <button className="outline w-20 h-10 rounded-full shadow-lg mx-2" onClick = {() => setColor("green")} style={{backgroundColor : "green"}}>Green</button>
                     <button className="outline w-20 h-10 rounded-full shadow-lg mx-2" onClick = {() => setColor("blue")} style={{backgroundColor : "blue"}}>Blue</button>
                     <button className="outline w-20 h-10 rounded-full shadow-lg mx-2" onClick = {() => setColor("white")} style={{backgroundColor : "white"}}>White</button>
